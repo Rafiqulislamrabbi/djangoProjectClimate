0c013bb2023b887b5d4e1a7b0ae9b866eaabdf92
ubuntu <rafiqul15-2546@diu.edu.bd>
2024-07-12 20:23:06 +0600

    dockerr remove

--- djangoProjectClimate/settings.py
+++ djangoProjectClimate/settings.py
@@ -5,8 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ore=h93yho^c#$!_j5v(chje=&i1drnlq!82ks!v9p8f^+$7hm'
